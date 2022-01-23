731d9e7911907163bb7b6e3b28f13b7d2249b01c
JakeLee <jake.lee@jalsalda.com>
2022-01-23 11:16:09 +0900

    start !

---
+++ src/client/webpack.common.js
@@ -0,0 +1,9 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+/** @type {import('webpack').Configuration} */
+const webpackCommonConfig = {
+  entry: "./src/index.ts",
+  module: {},
+};
+
+export default webpackCommonConfig;
